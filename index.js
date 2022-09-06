d6e8a1bc247f90d2b847039123711c04c3234c11
Seo Choi <seochoi56@gmail.com>
2022-09-06 11:58:20 -0400

    commit tree e153b3227aba050ccd3f82e10f072c6349c7b4e2

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    function nameMatch(match) {
+        if(name.toLowerCase() === match.toLowerCase()){
+            return true;
+        }
+    }
+    return drivers.filter(nameMatch);
+}
+
+function fuzzyMatch(drivers, letter) {
+    function initialMatch(match) {
+        let checker = ''
+        for (let i = 0; i < letter.length; i++ ) {
+            checker += match[i]
+        }
+        if(letter.toLowerCase() === checker.toLowerCase()) {
+            return true;
+        }
+    }
+    return drivers.filter(initialMatch)
+}
+
+function matchName(drivers, name) {
+    function objValueMatch(match) {
+        if(name.toLowerCase() === match.name.toLowerCase()) {
+            return true
+        }
+    }
+    return drivers.filter(objValueMatch)
+}
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+findMatching(drivers, "Bobby")
+fuzzyMatch(drivers, "Sa")
